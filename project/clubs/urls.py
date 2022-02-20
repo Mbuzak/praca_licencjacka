eadd492a18806446682983a388711d822302dd55
Mbuzak <58588944+Mbuzak@users.noreply.github.com>
2022-02-20 19:24:57 +0100

    New clubs urls file
    CRUD urls, index url, detail url and home url

---
+++ project/clubs/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from .views import *
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
+    path('create/', CreateClub.as_view(), name='create_club'),
+    path('<int:pk>/', DetailClub.as_view(), name='detail_club'),
+    path('update/<int:pk>/', UpdateClub.as_view(), name='update_club'),
+    path('delete/<int:pk>/', DeleteClub.as_view(), name='delete_club'),
+    path('join/', CreateClubMember.as_view(), name='join_club'),
+    path('<int:pk>/leave/', DeleteClubMember.as_view(), name='leave_club'),
+]
